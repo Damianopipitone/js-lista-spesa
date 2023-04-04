c1fac42cb4a92510486c2b83b2894ee0117cee0c
Damianopipitone <damianopipitone33@gmail.com>
2023-04-04 14:52:35 +0200

    Lista della spesa

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const list = ['Pane', 'Formaggio', 'Yogurt', 'Pomodori','Nutella'];
+
+/*for (let i = 0; i < list.length; i++) {
+    console.log(list[i]);
+}; */
+
+let i = 0;
+
+while (list[i]) {
+    
+    console.log(i+1 + '.' + list[i]);
+    i++;
+};
